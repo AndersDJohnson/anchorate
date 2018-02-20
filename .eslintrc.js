cbbc586e68d4dde4c90168168394821719f245a9
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-02-19 22:15:37 -0600

    chore: upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-    "extends": "standard",
-    "installedESLint": true,
-    "plugins": [
-        "standard",
-        "promise"
-    ]
-};
+  extends: 'standard',
+  plugins: [
+    'standard',
+    'promise'
+  ]
+}
