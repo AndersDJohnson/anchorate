e12783fac0d412b7a06bcc6941452db6d570d0e2
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-11-16 08:40:43 -0600

    chore: upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: 'standard',
-  plugins: [
-    'standard',
-    'promise'
-  ]
-}
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
